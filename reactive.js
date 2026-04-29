aeeb0ff117208bd5739b33623902fbf31990f3b5
Yarik <babenko.yaroslav2007@gmail.com>
2026-04-29 17:17:12 +0300

    Add Observable and output

--- reactive.js
+++ reactive.js
@@ -1,6 +1,6 @@
 class EventEmitter {
   constructor() {
-    this.listeners = {}; // { eventName: [fn1, fn2, ...] }
+    this.listeners = {}; 
   }
  
   on(event, fn) {
@@ -18,3 +18,39 @@ class EventEmitter {
     this.listeners[event].forEach(fn => fn(data));
   }
 }
+
+class Observable {
+  constructor(emitter, event) {
+    this.emitter = emitter;
+    this.event = event;
+  }
+ 
+  subscribe(fn) {
+    this.emitter.on(this.event, fn);
+    return () => this.emitter.off(this.event, fn); 
+  }
+}
+
+const emitter = new EventEmitter();
+
+const logger = data => console.log(`Logger get: "${data}"`);
+const notifier = data => console.log(`Notify notification: "${data}"`);
+ 
+emitter.on('message', logger);
+emitter.on('message', notifier);
+ 
+console.log("Emit 1: ");
+emitter.emit('message', 'Hi!');
+ 
+emitter.off('message', logger);
+ 
+console.log("Emit 2: ");
+emitter.emit('message', 'Second message');
+ 
+const obs = new Observable(emitter, 'message');
+const unsubscribe = obs.subscribe(d => console.log(`Obs get: "${d}"`));
+ 
+emitter.emit('message', 'observable');
+unsubscribe();
+emitter.emit('message', 'after unsubscribe');
+console.log("Obs no messages");
